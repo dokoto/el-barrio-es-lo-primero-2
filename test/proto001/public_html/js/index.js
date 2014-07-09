375bf9f647ac9ac896a858b0e96d30758b5242fe
administrador <administrador@io.local>
2014-07-09 21:30:03 +0200

    building sample 1

--- test/proto001/public_html/js/index.js
+++ test/proto001/public_html/js/index.js
@@ -1,51 +1,75 @@
-
 var game;
 var player1;
 var land1;
+var cursors;
 
 $(document).ready(function() {
    console.log("Begin Proto GAME 1");
-   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });            
+   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });            
 });
 
 function preload() {
-    game.load.atlas('petaxMovements', 'assets/petaxMovements.png', 'assets/petaxMovements.json');
-    game.load.image('landPhase1', 'assets/bg/fase_1_background.png');
+    game.load.atlas('petaxMovements', 'js/assets/petaxMovements.png', 'js/assets/petaxMovements.json');
+    game.load.image('landPhase1', 'js/assets/bg/fase_1_background.png');
 }
  
 function create() {
-    game.world.setBounds(-1000, -1000, 2000, 2000);
-    land1 = game.add.tileSprite(0, 0, 800, 600, 'landPhase1');
-    land1.fixedToCamera = true;
+    land1 = game.add.tileSprite(0, 0, 1679, 600, 'landPhase1');
+    game.world.setBounds(0, 0, 1679, 600);    
     
-    player1 = game.add.sprite(0, 0, 'petaxMovements');
+    player1 = game.add.sprite(((game.world.width - game.camera.width) / 2)+100, game.world.height, 'petaxMovements');
+    player1.scale.setTo(2.2, 2.2);
     player1.anchor.setTo(0.5, 0.5);
-    player1.animations.add('stop', [0, 1, 2, 3], 20, true);
-    player1.animations.add('walking', [4, 5, 6, 7], 20, true);
+    player1.animations.add('walking', [0, 1, 2, 3], 10, true);
+    player1.animations.add('stop', [4, 5, 6, 7], 8, true);
+    // Mirroring
+    //player1.scale.x = -1;
     
     game.physics.enable(player1, Phaser.Physics.ARCADE);
     player1.body.drag.set(0.2);
     player1.body.maxVelocity.setTo(400, 400);
+    player1.body.gravity.y = 300;
     player1.body.collideWorldBounds = true;
+
     
-    game.camera.follow(tank);
-    game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
-    game.camera.focusOnXY(0, 0);
+    game.camera.deadzone = new Phaser.Rectangle(10, 10, 800, 600);
+    game.camera.x = (game.world.width - game.camera.width) / 2;
+    //game.camera.follow(player1);
 
     cursors = game.input.keyboard.createCursorKeys();    
 }
- 
+
+
 function update() {
+    player1.body.velocity.x = 0;
+    if (Math.abs(game.camera.x-player1.x) <= player1.width)
+    {
+        game.camera.x -= 4;
+    }
+    else if (Math.abs(Math.abs((game.camera.x-player1.x)) - game.camera.width) <= player1.width)
+    {
+        game.camera.x += 4;
+    }
     if (cursors.right.isDown)
     {
-        player.body.velocity.x = 150;
-        player.animations.play('walking');
+        console.log("Right cursor presed");
+        //player1.animations.stop();
+        player1.body.velocity.x = 150;
+        player1.animations.play('walking');
+    }
+    else if (cursors.left.isDown)
+    {
+        console.log("Right cursor presed");
+        //player1.animations.stop();
+        player1.body.velocity.x = -150;
+        player1.animations.play('walking');
     }
     else
-    {
-        player.animations.stop();
-        player.animations.play('stop');
-        player.frame = 4;
-    }
-    
+        player1.animations.play('stop');
+}
+
+function render() {
+
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player1, 32, 200);    
 }
